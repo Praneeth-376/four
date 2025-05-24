ca692fe069321f0d5e42192e7ebab35a4ac3c0d2
Shanmukha-varun <shanmukhavarun47@gmail.com>
2025-05-24 11:02:17 +0530

    update App.js

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, React! 👋</h1>
+        <p>
+          This is a simple React component.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
